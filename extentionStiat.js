261dd3227070b557cc6a9f6c06a88c6435484453
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-05-20 15:03:55 +0100

    Update extentionStiat.js

--- extentionStiat.js
+++ extentionStiat.js
@@ -24,7 +24,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			{
 				name : 'Pessoa Racializada', //Category name to be used for feedback and logging.
 				title : {
-					media : {word : 'Negro'}, //Name of the category presented in the task.
+					media : {word : 'Pessoa Racializada''}, //Name of the category presented in the task.
 					css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
 					height : 4 //Used to position the "Or" in the combined block.
 				}, 
@@ -79,16 +79,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			},	
 			trialsByBlock : 
 			[//Each object in this array defines a block
-				{
-					instHTML : '', //Empty means we will create the inst from the instTemplate variable further below. 
-					block : 1, //The block variable is not used later, but could help the user. 
-					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
-					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
-					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
-					sharedAttTrials : 10, //Number of trials of the attribute that shares key with the category (in a mini block).
-					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
-					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
-				}, 
+			
 				{ 
 					instHTML : '', 
 					block : 2, 
