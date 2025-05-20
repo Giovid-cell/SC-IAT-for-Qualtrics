e4848ce6966e2823404dda3e5474c6a98aab544d
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-05-20 16:03:41 +0100

    Update extentionStiat.js

--- extentionStiat.js
+++ extentionStiat.js
@@ -80,16 +80,6 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			trialsByBlock : 
 			[//Each object in this array defines a block
 			
-					{
-					instHTML : '', //Empty means we will create the inst from the instTemplate variable further below. 
-					block : 1, //The block variable is not used later, but could help the user. 
-					//In each block, we can include a number of mini-blocks, to reduce repetition of same group/response.
-					miniBlocks : 1, //Set to 1 if don't need mini blocks. 0 will break the task.
-					singleAttTrials : 10, //Number of trials of the attribute that does not share key with the category (in a mini block).
-					sharedAttTrials : 10, //Number of trials of the attribute that shares key with the category (in a mini block).
-					categoryTrials : 0 // Number of trials of the category (in a mini-block). If 0, the label does not appear.
-					//Note: if no category trials, then attribute1, the one on the left, is considered the single attribute.
-				},
 				{ 
 					instHTML : '', 
 					block : 2, 
