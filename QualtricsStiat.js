333bece9d512490a4d85e334f753614f4dfd9114
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-05-20 16:01:29 +0100

    Update QualtricsStiat.js

--- QualtricsStiat.js
+++ QualtricsStiat.js
@@ -1,6 +1,6 @@
 // https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@main/QualtricsStiat.js
 
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics/extentionStiat.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@eb0e88d6cf197675ceeb7b6f0d1aa1f1ce614f5f/extentionStiat.js'], function(APIConstructor, stiatExtension){
   var API = new APIConstructor();
   return stiatExtension({
     category : { 
