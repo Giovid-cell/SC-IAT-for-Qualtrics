8891c044b012d5928a045a8a218663b66a00cdab
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-09-27 16:56:24 +0200

    Create Qualtrics_st3.js

---
+++ SC_IAT_ita_study3/scripts/Qualtrics_st3.js
@@ -0,0 +1,61 @@
+// https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics/QualtricsStiat.js
+
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@e4848ce6966e2823404dda3e5474c6a98aab544d/extentionStiat.js'], function(APIConstructor, stiatExtension){
+  var API = new APIConstructor();
+  return stiatExtension({
+    category : { 
+      name : 'Pessoa Racializada',
+      title : {
+        media : {word : 'Pessoa Racializada'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      }, 
+      media : [
+        {image : 'b1.jpg'}, 
+        {image : 'b2.jpg'},
+        {image : 'b3.jpg'},
+        {image : 'b4.jpg'},
+        {image : 'b5.jpg'},
+        {image : 'b6.jpg'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    attribute1 : {
+      name : 'Evitamento',
+      title : {
+        media : {word : 'Evitamento'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      },
+      media : [
+        {word: 'Evito'},
+        {word: 'Fujo'},
+        {word: 'Afasto-me'},
+        {word: 'Desvio-me'},
+        {word: 'Rejeito'},
+        {word: 'Recuo'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    attribute2 : {
+      name : 'Aproximação',
+      title : {
+        media : {word : 'Aproximação'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      },
+      media : [
+        {word: 'Aproximo-me'},
+        {word: 'Chego-me'},
+        {word: 'Aproximação'},
+        {word: 'Aproximar-se'},
+        {word: 'Tocar'},
+        {word: 'Aconchegar'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    base_url : {
+      image : 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@main/images/'
+    }
+  });
+});
