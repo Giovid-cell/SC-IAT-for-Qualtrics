e0aee5cdf57f62b2429d4e9408036d802b030ef8
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-10-08 18:54:29 +0200

    Create Qualtrics_st3_ITA.js

---
+++ SC_IAT_ita_study3/scripts/scripts_ITA/Qualtrics_st3_ITA.js
@@ -0,0 +1,58 @@
+// https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics/QualtricsStiat.js
+
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@093c5022abc61343569940ba8baad87a6e95ec05/SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js'], function(APIConstructor, stiatExtension){
+  var API = new APIConstructor();
+  return stiatExtension({
+    category : { 
+      name : 'Nome Paziente',
+      title : {
+        media : {word : 'Nome Paziente'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      }, 
+      media : [ 
+				{word: 'Paolo'},
+				{word: 'Tosato'},
+				{word: 'Paolo Tosato'},
+				{word: 'P.T.'},
+				{word: 'Tosato Paolo'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    attribute1 : {
+      name : 'Ignorare',
+      title : {
+        media : {word : 'Ignorare'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      },
+      media : [ 
+				{word: 'ostacolare'},
+				{word: 'trascurare'},
+				{word: 'ignorare'},
+				{word: 'dimenticare'},
+				{word: 'tralasciare'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    attribute2 : {
+      name : 'Sostenere',
+      title : {
+        media : {word : 'Sostenere'},
+        css : {color:'#31b404','font-size':'2em'},
+        height : 7
+      },
+      media : [
+				{word: 'aiutare'},
+				{word: 'assistere'},
+				{word: 'soccorrere'},
+				{word: 'sostenere'},
+				{word: 'appoggiare'}
+      ],
+      css : {color:'#31b404','font-size':'3em'}
+    },
+    base_url : {
+      image : 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@main/images/'
+    }
+  });
+});
