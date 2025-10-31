86f898e403966ef7fa6e6038f37b2f67b4dd4eaf
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-10-31 15:24:29 +0100

    Update Qualtrics_st3_ITA.js

--- SC_IAT_ita_study3/scripts/scripts_ITA/Qualtrics_st3_ITA.js
+++ SC_IAT_ita_study3/scripts/scripts_ITA/Qualtrics_st3_ITA.js
@@ -1,6 +1,6 @@
 // https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics/QualtricsStiat.js
 
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@1ae229f02daede85a36779488fd8ed54539743a7/SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/Giovid-cell/SC-IAT-for-Qualtrics@24c8cd48940b3f52e7228a6f0914793c696fd77e/SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js'], function(APIConstructor, stiatExtension){
   var API = new APIConstructor();
   return stiatExtension({
     category : { 
