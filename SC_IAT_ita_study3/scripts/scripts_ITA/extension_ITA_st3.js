fcde29eece4780256a188679146e5b582e5dcdff
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-10-31 15:57:35 +0100

    Update extension_ITA_st3.js

--- SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js
+++ SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js
@@ -537,9 +537,9 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			error : [{
 				data:{handle:'error'}, location: {top: 70}, css:{color:'red','font-size':'4em'}, media: {word:'X'}, nolog:true
 			}], 			
-			correct : [{
-        data:{handle:'correct'}, location: {top: 70}, css:{color:'green','font-size':'4em'}, media: {word:'O'}, nolog:true
-    		}],
+			correct: [{
+    data: {handle: 'correct'}, location: {top: 75}, css: {color: 'green', 'font-size': '4em', 'font-weight': 'bold', 'text-shadow': '0 0 2px #000'}, media: {word: 'O'}, nolog: true                          
+			}],
 			dummyForLog : [{
 				data:{name:'dummyForLog', alias:'dummyForLog'}, 
 				location:{left:99}, media:{word:' '}
