988824ede572cf40c5c7b7b1d435b6ed361e6d81
Giovid-cell <giovanni.milauro@studenti.unipd.it>
2025-10-31 15:21:18 +0100

    Update extension_ITA_st3.js

--- SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js
+++ SC_IAT_ita_study3/scripts/scripts_ITA/extension_ITA_st3.js
@@ -149,7 +149,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				instTemplateCategoryLeft: '<div style="width:100%; height:100%; font-family:arial; padding:38px 0 0 38px;">' +
 				  '<div style="display:flex; justify-content:flex-start; align-items:flex-start; width:100%; height:100%;">' +
 				    '<div style="width:80%; max-width:700px; font-size:18px; line-height:1.35em; text-align:left;">' +
-				      '<p style="margin-bottom:6px;">Le chiediamo di partecipare a secondo esperimento.</p>' +
+				      '<p style="margin-bottom:6px;">Le chiediamo di partecipare ad un esperimento.</p>' +
 				      '<p style="margin-bottom:6px;">Disponga l\'indice o il medio sul tasto <b>"W"</b> e sul tasto <b>"P"</b>.</p>' +
 				      '<p style="margin-bottom:6px;">Al centro dello schermo compariranno stimoli appartenenti alle categorie visualizzate in alto, a destra e a sinistra.</p>' +
 				      '<p style="margin-bottom:6px;">Se lo stimolo appartiene a una categoria a <b>SINISTRA</b>, prema <b>"W"</b>;<br>' +
@@ -166,7 +166,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 				instTemplateCategoryRight: '<div style="width:100%; height:100%; font-family:arial; padding:38px 0 0 38px;">' +
 				  '<div style="display:flex; justify-content:flex-start; align-items:flex-start; width:100%; height:100%;">' +
 				    '<div style="width:80%; max-width:700px; font-size:18px; line-height:1.35em; text-align:left;">' +
-				      '<p style="margin-bottom:6px;">Le chiediamo di partecipare a secondo esperimento.</p>' +
+				      '<p style="margin-bottom:6px;">Le chiediamo di partecipare ad un esperimento.</p>' +
 				      '<p style="margin-bottom:6px;">Disponga l\'indice o il medio sul tasto <b>"W"</b> e sul tasto <b>"P"</b>.</p>' +
 				      '<p style="margin-bottom:6px;">Al centro dello schermo compariranno stimoli appartenenti alle categorie visualizzate in alto, a destra e a sinistra.</p>' +
 				      '<p style="margin-bottom:6px;">Se lo stimolo appartiene a una categoria a <b>SINISTRA</b>, prema <b>"W"</b>;<br>' +
